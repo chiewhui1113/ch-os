b8947bc4b51cbd8a74bac09bd7b276ab67f49b17
Chiew Hui <chiewhui.lim@gmail.com>
2026-06-25 13:56:11 +1000

    Post 02: A Minimal Rust Kernel

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,25 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[unsafe(no_mangle)] // Don't mangle the name of this function 
 // ! means not allowed to ever return 
 // Entry point invoked by the os or bootloader
 pub extern "C" fn _start() -> ! {
-    // The linker looks for a function named `_start` by default 
+    // First address that maps to the screen
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            // Byte 1: character
+            *vga_buffer.offset(i as isize * 2) = byte;
+            // Byte 2: color
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    // Keeps OS alive
     loop {}
 }
 
